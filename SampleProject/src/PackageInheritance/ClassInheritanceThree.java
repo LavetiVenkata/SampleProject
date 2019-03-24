8d5b40f55b0971d60763696bd29e5da2ce2dc605
Dell <Dell@DESKTOP-6AKBNI5>
2019-03-24 10:16:19 +0530

    mycomments

--- SampleProject/src/PackageInheritance/ClassInheritanceThree.java
+++ SampleProject/src/PackageInheritance/ClassInheritanceThree.java
@@ -3,12 +3,11 @@ package PackageInheritance;
 public class ClassInheritanceThree {
 	
 	public void add(){
-		System.out.println("adding");
-		System.out.println("Hello");
+		
 		int a = 99;
 		int b = 90;
-		int c = 87;
-		int d = a - b;
+		int c = a + b;
+		System.out.println("Testing");
 		System.out.println(c);
 	}
 

92897fbfc1b5fcf751d74efc7e64cfafca267df2
Owner <Owner@DESKTOP-399M7VK>
2019-10-20 10:27:04 -0500

    my init commit

--- SampleProject/src/PackageOne/Class_Four.java
+++ SampleProject/src/PackageOne/Class_Four.java
@@ -5,10 +5,11 @@ public class Class_Four {
 	public static void main(String[] args) {
 		
 		System.out.println("Helloo");
-		System.out.println("Hi");
+		
 		//comments
 		
 		System.out.println("my first commit");
+		System.out.println("Some change");
 
 	}
 

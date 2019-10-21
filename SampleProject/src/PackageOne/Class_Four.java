36f660e8e04b126a1849d4cc985774581faec999
ruby_ <ruby_@DESKTOP-T33HM3L>
2019-10-20 22:30:41 -0700

    nna

--- SampleProject/src/PackageOne/Class_Four.java
+++ SampleProject/src/PackageOne/Class_Four.java
@@ -5,12 +5,6 @@ public class Class_Four {
 	public static void main(String[] args) {
 		
 		System.out.println("Helloo");
-		
-		//comments
-		
-		System.out.println("my first commit");
-		System.out.println("Some change");
-
 	}
 
 }
